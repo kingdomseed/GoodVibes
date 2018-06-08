46a4dc55a7b84eac26ca97faa0968ecdfc737853
kingdomseed <kingdomseed@gmail.com>
2018-06-08 10:51:54 -0500

    ListView is complete and now I've started working on passing data back to Play. I've got a toast working in an OnItemClickListener so progress!

--- app/src/main/java/com/holtnet/goodvibes/LibraryActivity.java
+++ app/src/main/java/com/holtnet/goodvibes/LibraryActivity.java
@@ -2,44 +2,50 @@ package com.holtnet.goodvibes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class LibraryActivity extends AppCompatActivity {
+
+    private ArrayList<Song> songs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_activity);
 
-        ArrayList<Song> songs = new ArrayList<>();
 
-        songs.add(new Song("Adieu","Coeur de Pirate","Coeur de Pirate", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("","","", R.drawable.album1));
-        songs.add(new Song("Adieu","Coeur de Pirate","Coeur de Pirate", R.drawable.album1));
+
+        songs.add(new Song("Comme des Enfants","Coeur de Pirate","Coeur de Pirate", R.drawable.album1));
+        songs.add(new Song("Pour un infidèle","Coeur de Pirate","Coeur de Pirate", R.drawable.album1));
+        songs.add(new Song("Ensemble","Coeur de Pirate","Coeur de Pirate", R.drawable.album1));
+        songs.add(new Song("Francis","Coeur de Pirate","Coeur de Pirate", R.drawable.album1));
+        songs.add(new Song("Adieu","Blonde","Coeur de Pirate", R.drawable.album2));
+        songs.add(new Song("Golden Baby","Blonde","Coeur de Pirate", R.drawable.album2));
+        songs.add(new Song("Place de la République","Blonde","Coeur de Pirate", R.drawable.album2));
+        songs.add(new Song("Crier tout bas","Roses","Coeur de Pirate", R.drawable.album3));
+        songs.add(new Song("Drapeau Blanc","Roses","Coeur de Pirate", R.drawable.album3));
+        songs.add(new Song("Oublie-moi","Roses","Coeur de Pirate", R.drawable.album3));
+        songs.add(new Song("Prémonition","En cas de tempête, ce jardin sera fermé","Coeur de Pirate", R.drawable.album4));
+        songs.add(new Song("Somnambule","En cas de tempête, ce jardin sera fermé","Coeur de Pirate", R.drawable.album4));
+        songs.add(new Song("Dans la nuit","En cas de tempête, ce jardin sera fermé","Coeur de Pirate", R.drawable.album4));
 
         SongAdapter songAdapter = new SongAdapter(this, songs);
 
         ListView listView = findViewById(R.id.list);
         listView.setAdapter(songAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(LibraryActivity.this, songs.get(position).toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/holtnet/goodvibes/SongAdapter.java
+++ app/src/main/java/com/holtnet/goodvibes/SongAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -37,6 +38,13 @@ public class SongAdapter extends ArrayAdapter {
 
         TextView albumNameTextView = listItemView.findViewById(R.id.AlbumName);
         albumNameTextView.setText(currentSong.getmAlbumName());
+
+        TextView artistNameTextView = listItemView.findViewById(R.id.ArtistName);
+        artistNameTextView.setText(currentSong.getmArtistName());
+
+        ImageView albumArtImageView = listItemView.findViewById(R.id.AlbumArt);
+        albumArtImageView.setImageResource(currentSong.getmAlbumArt());
+
 
         return listItemView;
     }
